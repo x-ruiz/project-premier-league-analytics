a17602c250a416f4fff6eabafbf12e9e15d2b537
Xavier Ruiz <xavierruiz@Xaviers-MacBook-Pro.local>
2025-03-23 05:02:43 -0500

    generate avro schema and first pass at file generation

--- java-app/ingestion/src/main/java/com/premierleagueanalytics/ingestion/SparkHttpToAvro.java
+++ java-app/ingestion/src/main/java/com/premierleagueanalytics/ingestion/SparkHttpToAvro.java
@@ -6,6 +6,18 @@ import java.net.http.HttpResponse;
 import java.net.URI;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
+import java.time.Instant;
+import java.io.File;
+
+
+// Avro Related
+import com.premierleagueanalytics.ingestion.TeamInfo;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.specific.SpecificDatumWriter;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 // Exception Imports
 import java.io.IOException;
@@ -19,13 +31,56 @@ class SparkHttpToAvro {
         System.out.println("Spark Http To Avro");
     }
 
+    // maybe separate it out into its own class? Idk how this will scale though.
+    // possibly a models class? Benefit of one class per model is we can combine related
+    // endpoints and prevent file spread.
     public void getTeams() {
         try {
             String response = httpGetRequest("/v4/teams");
-            System.out.println("Teams Response:\n" + response);
+            JsonNode rootNode = parseJson(response);
+            JsonNode teamsNode = rootNode.get("teams");
+
+            DatumWriter<TeamInfo> datumWriter = new SpecificDatumWriter<>(TeamInfo.class);
+            try (DataFileWriter<TeamInfo> dataFileWriter = new DataFileWriter<>(datumWriter)) {
+                dataFileWriter.create(TeamInfo.getClassSchema(), new File("teams.avro"));
+
+                if (teamsNode.isArray()) {
+                    for (JsonNode jsonNode : teamsNode) {
+                        TeamInfo teamInfo = createTeamInfo(jsonNode);
+                        dataFileWriter.append(teamInfo);
+                    }
+                }
+            }
+
+//            System.out.println("Teams Response:\n" + response);
         } catch (URISyntaxException | InterruptedException | IOException e) {
             System.err.println("Request to get teams failed with error: " + e.getMessage());
+        } catch (Exception e) {
+            System.err.println("Failed to parse json with error: " + e.getMessage());
         }
+
+
+    }
+
+    private JsonNode parseJson(String jsonString) throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readTree(jsonString);
+    }
+
+    private TeamInfo createTeamInfo(JsonNode node) {
+        return TeamInfo.newBuilder()
+                .setId(node.get("id").asInt())
+                .setName(node.get("name").asText())
+                .setShortName(node.get("shortName").asText())
+                .setTla(node.get("tla").asText())
+                .setCrest(node.get("crest").asText())
+                .setAddress(node.get("address").asText())
+                .setWebsite(node.get("website").asText())
+                .setFounded(node.get("founded").asInt())
+                .setClubColors(node.get("clubColors").asText())
+                .setVenue(node.get("venue").asText())
+                .setLastUpdated(Instant.parse(node.get("lastUpdated").asText()).toEpochMilli())
+                .build();
     }
 
     // TODO: Extract this method into its own API class
